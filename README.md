934bdb5a89bbb993e0fe386f49c369b4fd46410b
planktoon1 <fkoefkoe@live.dk>
2018-11-21 09:32:10 +0100

    US1: Grundarkitektur, US9: Navigation
    husk at køre "npm install"

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# lopperoglandstil
-#blessed
+# Website
+
---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.localMongoDB = 'mongodb://jokemaster:fred123@ds155663.mlab.com:55663/joke_service';
+config.localPort = 8080;
+
+config.mlabMongoDB = ''; // TODO
+
+module.exports = config;
