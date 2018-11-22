e9cc24047c8ec940ab7665a4b158be6ce21df88a
Simon Grøn <simon.gron@live.dk>
2018-11-22 11:09:24 +0100

    You can now login

--- routes/admin.js
+++ routes/admin.js
@@ -30,7 +30,7 @@ console.log('input name: ' + name + password)
     router.get('/session', function (req, res) {
         const name = req.session.name;
         if(name) {
-            res.render(`session.hbs`);
+            res.render(`session.hbs`,{name});
         }else{
             res.render('loginFail')
         }
@@ -43,7 +43,7 @@ console.log('input name: ' + name + password)
             if (err) {
                 console.log(err);
             } else {
-                res.redirect('/');
+                res.redirect('/admin');
             }
         });
     });
