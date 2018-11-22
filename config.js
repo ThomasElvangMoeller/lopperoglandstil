bc68741fddf48b95703cbc70fc48d9706f3bb487
Thomas Møller <thomas.niceguy@gmail.com>
2018-11-22 22:20:20 +0100

    RESTful api unit test. Start ikke testapp i test mappen, bare kør test filen direkte

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const config = {};
 
 config.localMongoDB = 'mongodb://sysadmin:simonlugterafost123@ds113454.mlab.com:13454/lopperoglandstil';
 config.localPort = 8080;
+config.testMongoDB = 'mongodb://testmaster:tester123@ds115094.mlab.com:15094/lopperoglandstiltests';
 
 config.mlabMongoDB = ''; // TODO
 
