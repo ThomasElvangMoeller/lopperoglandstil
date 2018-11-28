7611fd4431481291a2a30c9291609d90f522f317
Simon Grøn <simon.gron@live.dk>
2018-11-28 09:36:33 +0100

    Revert "DELETE"
    This reverts commit 3405e2d03c7599a06af6b4f5f904c8a2b3221312.

---
+++ controllers/controller.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const Produkt = require("../models/Produkt");
+
+// Create a product and save it to the database
+// TODO
+exports.createProdukt = () => {
+  let produkt = new Produkt({
+    
+  });
+  return produkt.save();
+};
+
+// Find and return all products in the database
+// TODO
+exports.getProdukts = () => {
+  return Produkt.find().exec();
+};
+
+//------------------------------------------------------------------------
+const Login = require("../models/Login");
+
+// Create a login and save it to the database
+// TODO
+exports.createLogin = (username, password) => {
+    let login = new Login({
+        username: username,
+        password: password
+    });
+    return login.save();
+};
+
+// Find and return all logins in the database
+// TODO
+exports.getLogins = (name,pass) => {
+    return Login.find({username: name, password: pass}).exec();
+};
