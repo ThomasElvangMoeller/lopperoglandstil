9fd19279554763daffbf7f088c3ae984c5c35ddf
planktoon1 <fkoefkoe@live.dk>
2018-11-30 12:48:47 +0100

    katalog reserver popup

--- public/scripts/scriptKatalog.js
+++ public/scripts/scriptKatalog.js
@@ -7,4 +7,21 @@ onload = async () => {
 
     const compiledKatalogTemplate = Handlebars.compile(katalogTemplateText);
     document.getElementById("products").innerHTML = compiledKatalogTemplate({product: katalogProductsJSON});
+
+
+    
+    const reserverButtons = document.getElementsByClassName("reservation");
+    const reservationWindow = document.getElementById("reservationWindow");
+    document.getElementById("closeBtn").onclick = () => {reservationWindow.style.display = "none";}
+
+    for (btn of reserverButtons) {
+        btn.onclick = () => {
+            //reservationWindow.style.display = "block";
+
+            //document.getElementById("reserverProduct").innerHTML = `Produkt: ${button.dataset.product}`;
+
+            console.log(`Produkt: ${btn.dataset.product}` );
+        }
+    }
+    
 };
