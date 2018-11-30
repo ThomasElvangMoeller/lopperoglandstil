edf41f34630f92518e8a331c13bf791f7c3ca434
Ole Fosdal Andresen <olefosdal@gmail.com>
2018-11-30 09:40:12 +0100

    produkt beskrivelse fixed
    Muligt at klikke på et produkt og vise dets informationer

--- public/scripts/scriptProduct.js
+++ public/scripts/scriptProduct.js
@@ -1,21 +1,15 @@
 onload = async () => {
-    const katalogProducts = await fetch('/api/produkter');
+
+    const productURL = window.location.href;
+    const splitURL = productURL.split('?');
+    const ID = splitURL[1];
+
+    const katalogProducts = await fetch('/api/produkter/' + ID);
     const katalogProductsJSON = await katalogProducts.json();
-
-    Handlebars.registerHelper('ifCond', function(v1, v2, options) {
-
-        // var name = v1 + '';
-
-        if(v1 === v2) {
-            return options.fn(this);
-        }
-        console.log('nope');
-        return options.inverse(this);
-    });
 
     const katalogTemplate = await fetch('/templates/productDescription.hbs');
     const katalogTemplateText = await katalogTemplate.text();
 
     const compiledKatalogTemplate = Handlebars.compile(katalogTemplateText);
-    document.getElementById("product-container").innerHTML = compiledKatalogTemplate({product: katalogProductsJSON});
+    document.getElementById("product-container").innerHTML = compiledKatalogTemplate(katalogProductsJSON);
 };
