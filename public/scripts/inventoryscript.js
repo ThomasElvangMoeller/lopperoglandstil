e80efa74eeb93bb7936412275ed466dc097368ea
Thomas Møller <thomas.niceguy@gmail.com>
2018-12-02 22:52:26 +0100

    Kan redigere alle felter, mangler billede halløj

--- public/scripts/inventoryscript.js
+++ public/scripts/inventoryscript.js
@@ -12,7 +12,8 @@ onload = async () =>{
 
     for (let e of product){
         for (let c of e.categories){
-            categories.add(c);
+            if(/\S/.test(c))
+            categories.add(c.trim());
         }
     }
 
@@ -54,25 +55,121 @@ onload = async () =>{
             currentRow = row;
             document.getElementById('inventory_specs').innerHTML = compiledProductTemplate({product: product[currentRow-1]})
         }
-        console.log(currentRow);
+        // console.log(currentRow);
     }
 
     async function deleteProduct(id) {
         let areYouSure = confirm('Er du sikker på at du vil slette valgte produkt? \nDette er permanent og kan ikke fortrydes');
         if(areYouSure) {
             await fetch('/api/produkter/' + id, {method: 'DELETE'});
-            productsJSON = await fetch('/api/produkter');
-            product = await productsJSON.json();
-            document.getElementById('inventory_main').innerHTML = await compiledTableTemplate({product});
-            addFunctionToRows();
+            updateTable();
             document.getElementById('inventory_specs').innerHTML = compiledProductTemplate({product: product[currentRow - 2]})
         }
     }
 
     async function editProduct(id) {
+        let tempCategories = new Set();
+
+        for(let c of product[currentRow-1].categories){
+            if(/\S/.test(c)) //ensure the category is more than just whitespace
+                tempCategories.add(c);
+        }
+
         document.getElementById('inventory_specs').innerHTML = compiledProductEdit({product: product[currentRow-1]});
+
+        document.getElementById('exit').onclick = function () {
+            currentlyEditing = false;
+            document.getElementById('inventory_specs').innerHTML = compiledProductTemplate({product: product[currentRow-1]})
+        };
+
+        let categorieshtml = ""; //This shows already made categories
+        categories.forEach(category => {
+            categorieshtml += '<option value="'+category+'">';
+        });
+        document.getElementById('categories_datalist').innerHTML = categorieshtml;
+
+        let productName = document.getElementById('inventory_product_edit_name');
+        let productDesc = document.getElementById('inventory_product_edit_description');
+        let productAmount = document.getElementById('inventory_product_edit_amount');
+        let productReservedAmount = document.getElementById('inventory_product_edit_reservedamount');
+        let productUnique = document.getElementById('inventory_product_edit_unique');
+        let productPrice = document.getElementById('inventory_product_edit_price');
+        let productDiscount = document.getElementById('inventory_product_edit_discount');
+
+        let productCategories = document.getElementById('inventory_product_edit_categories');
+        let productCategoriesAdd = document.getElementById('categories_add');
+        let productCategoriesRemove = document.getElementById('categories_remove');
+        let productCategoriesInput = document.getElementById('categories_input');
+
+        productCategoriesAdd.onclick = function () {
+            let category = productCategoriesInput.value.trim();
+            if(/\S/.test(category)){
+
+                categories.add(category);
+                tempCategories.add(category);
+
+                let approved = "";
+                tempCategories.forEach(elem => {
+                    approved += '<li id="individual_category_'+elem+'">'+elem+'</li>'
+                });
+                productCategories.innerHTML = approved;
+            }
+        };
+
+        productCategoriesRemove.onclick = function () {
+            let category = productCategoriesInput.value.trim();
+            if(/\S/.test(category) && tempCategories.has(category)){
+
+                tempCategories.delete(category);
+
+                let approved = "";
+                tempCategories.forEach(elem => {
+                    approved += '<li id="individual_category_'+elem+'">'+elem+'</li>'
+                });
+                productCategories.innerHTML = approved;
+            }
+        };
+
+        document.getElementById('save').onclick = async function (){
+            let data = {};
+            let p = product[currentRow-1];
+
+            if(productName.value !== p.name){data.name = productName.value}
+            if(productDesc.value !== p.description){data.desc = productDesc.value}
+            if(productAmount.value !== p.amount){data.amount = productAmount.value}
+            if(productReservedAmount.value !== p.reservedAmount){data.reservedAmount = productReservedAmount.value}
+            if(productUnique.checked !== p.unique){data.unique = productUnique.checked}
+            if(productPrice.value !== p.price){data.price = productPrice.value}
+            if(productDiscount.value !== p.discount){data.discount = productDiscount.value}
+            let newCategories = [];
+            tempCategories.forEach(elem => {newCategories.push(elem)});
+            if(newCategories !== p.categories) {data.categories = newCategories}
+
+
+            let status = await fetch('/api/produkter/'+ id, {method: 'PUT',
+                    headers:{'Content-Type':'application/json'},
+                    body:JSON.stringify(data)
+            });
+
+            let statusJSON = await status.json();
+
+            if(statusJSON.success){
+                document.getElementById('edit_status').innerText = 'Produkt opdateret';
+                updateTable();
+            }else{
+                document.getElementById('edit_status').innerText = 'Upload fejlede';
+            }
+        };
     }
 
+    async function updateTable() {
+        productsJSON = await fetch('/api/produkter');
+        product = await productsJSON.json();
+        document.getElementById('inventory_main').innerHTML = await compiledTableTemplate({product});
+        addFunctionToRows();
+    }
+
+    // console.log(categories);
 
 };
 
