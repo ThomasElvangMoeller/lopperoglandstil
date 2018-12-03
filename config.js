05666f96dcebe088476d9bb5b82a70d35a7f3102
planktoon1 <fkoe90@gmail.com>
2018-12-03 16:45:43 +0100

    Begyndte at lave reservations backend

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 const config = {};
 
-config.localMongoDB = 'mongodb://sysadmin:simonlugterafost123@ds113454.mlab.com:13454/lopperoglandstil';
+config.mlabMongoDB = 'mongodb://sysadmin:simonlugterafost123@ds113454.mlab.com:13454/lopperoglandstil';
 config.localPort = 8080;
 config.testMongoDB = 'mongodb://testmaster:tester123@ds115094.mlab.com:15094/lopperoglandstiltests';
 
-config.mlabMongoDB = ''; // TODO
+config.transporter = { //TODO
+    service: 'gmail',
+    auth: {
+      user: '',
+      pass: ''
+    }
+};
 
 module.exports = config;
