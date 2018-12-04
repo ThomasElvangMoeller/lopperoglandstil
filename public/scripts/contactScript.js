ffca47d093de150375e4a4c1b47e5be824733ecd
Thomas Møller <thomas.niceguy@gmail.com>
2018-12-04 14:49:37 +0100

    contact færdig

--- public/scripts/contactScript.js
+++ public/scripts/contactScript.js
@@ -1,28 +1,33 @@
-onload = async () =>{
+onload = () =>{
 
 
-    const name = document.getElementById('contact_name').value;
-    const mail = document.getElementById('contact_mail').value;
-    const phone = document.getElementById('contact_phone').value;
-    const message = document.getElementById('contact_message').value;
     const submit = document.getElementById('button_submit');
 
+    submit.onclick = sendMessage;
 
-
-
-    submit.onclick = async () =>{
+    async function sendMessage() {
+        const name = document.getElementById('contact_name');
+        const mail = document.getElementById('contact_mail');
+        const phone = document.getElementById('contact_phone');
+        const message = document.getElementById('contact_message');
+        const response = document.getElementById('contact_status');
 
         let postBody = {
-          from: mail,
-          html:`Navn: '${name}' <br> Email: '${mail} <br> Telefon: '${phone} <br> Besked: <br> '${message}`
+            from: mail,
+            html:`Navn: '${name.value}' <br> Email: '${mail.value} <br> Telefon: '${phone.value} <br> Besked: <br> '${message.value}`
         };
 
-        let status = await fetch('/api/email/kontakt',
-            {method: 'POST', headers:{"Content-Type":"application/json"}, body:JSON.stringify(postBody)});
-        console.log(status.status);
-        console.log(status);
+        let status = await fetch('/api/email/reservation', {method: 'POST',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify(postBody)
+        });
 
+        let statusJSON = await status.json();
+
+        if(statusJSON.success){
+            response.innerText = 'Besked Sendt!'
+        }else {
+            response.innerText = 'Der skete en fejl'
+        }
     }
-
-
 };
