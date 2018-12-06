fe2cfdc024c42b522ee5b6030efb45ca39259bf3
Frank <frankjensen156@gmail.com>
2018-12-06 12:39:39 +0100

    animation til mobile katalog menu

--- public/scripts/scriptKatalog.js
+++ public/scripts/scriptKatalog.js
@@ -40,6 +40,15 @@ onload = async () => {
 
         const compiledKatalogTemplate = Handlebars.compile(katalogTemplateText);
         document.getElementById("products").innerHTML = compiledKatalogTemplate({product: katalogProductsJSON});
+    }else if(ID == "Special"){
+        const katalogProducts = await fetch('/api/produkter');
+        const katalogProductsJSON = await katalogProducts.json();
+
+        const katalogTemplate = await fetch('/templates/produktUdstilling.hbs');
+        const katalogTemplateText = await katalogTemplate.text();
+
+        const compiledKatalogTemplate = Handlebars.compile(katalogTemplateText);
+        document.getElementById("products").innerHTML = compiledKatalogTemplate({product: katalogProductsJSON});
     }
     else{
         const katalogProducts = await fetch('/api/produktkategorier/' + ID);
