4e34f875fdec1d43313d0e596d6c228f6d7dd05c
planktoon1 <fkoe90@gmail.com>
2018-12-06 09:26:37 +0100

    Comments in the controller

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,9 @@
 "use strict";
 const config = require('../config');
-const Product = require("../models/Product");
 const mongooseId = require('mongoose').Types.ObjectId;
 const fs = require('fs');
 
+const Product = require("../models/Product");
 //--------------------Products------------------------
 // Create a product and save it to the database
 exports.createProduct = (name, desc, amount, categories, price, unique = false) => {
@@ -19,6 +19,7 @@ exports.createProduct = (name, desc, amount, categories, price, unique = false) 
   return product.save();
 };
 
+// Get a product from the database with a specified id
 exports.getProduct = (id) => {
   return Product.findById(id).exec();
 }
@@ -33,6 +34,8 @@ exports.updateProduct = (id, reqbody) => {
   let updates = {};
   updates.updated = Date.now();
 
+  /* This is done to make the function flexible. Instead of having a function
+      for every field on the product we only have this one*/
   if (reqbody.name) { updates.name = reqbody.name};
   if (reqbody.desc) { updates.description = reqbody.desc};
   if (reqbody.amount) { updates.amount = reqbody.amount};
@@ -42,12 +45,18 @@ exports.updateProduct = (id, reqbody) => {
   if (reqbody.discount) { updates.discount = reqbody.discount};
   if (reqbody.reservedAmount) { updates.reservedAmount = reqbody.reservedAmount};
 
+  // updates the product with the specified id to match the fields in the updates objects
   Product.updateOne({_id: id}, updates).exec();
 }
 
 // Delete a specific product and the pictures of the product
 exports.deleteProduct = async (id) => {
+  // Find the object we wanna delete
   let product = await Product.findOne({_id:id});
+
+  /* It's not enough to delete the product from the database, because our pictures are saved
+      in the server application storage. This for-loop takes care of deleting the pictures
+      from the app storage*/
   for (let pic of product.pictures) {
     fs.exists(`./public/uploads/${pic}.jpg`, function(exists) {
       if(exists) {
@@ -59,13 +68,22 @@ exports.deleteProduct = async (id) => {
       }
     });
   }
+  // After all the pictures are deleted, we delete the product from the database:
   Product.deleteOne({_id:id}).exec();
 }
 
+/* Deletes specified pictures from a specified product. This takes care of updating the product
+    refrences to the pictures, aswell as deleting the pictures from the application storage*/
 exports.deletePicturesFromProduct = async (product_id, picture_ids) => {
 
-  let product = await Product.findOneAndUpdate({_id: mongooseId(product_id)}, {$pullAll: {pictures: picture_ids}, updated: Date.now()} ).exec();
+  // Remove the refrences to the pictures on the product in the database
+  let product = await Product.findOneAndUpdate({
+    _id: mongooseId(product_id)}, 
+    {$pullAll: {pictures: picture_ids}, //delete the refrences from the array
+    updated: Date.now()
+  }).exec();
 
+  // Delete the pictures from the application storage
   if (product) {
     for (let pic of picture_ids) {
       fs.exists(`./public/uploads/${pic}.jpg`, function(exists) {
@@ -94,9 +112,11 @@ exports.addPictures = async (product_id, picture_ids) => {
 
 //--------------------Category------------------------
 const Category = require("../models/Category");
+
 // Create a category and save it to the database
 exports.createCategory = (name) => {
   let category = new Category({
+     // Note that the name is the document id. meaning every category name has to be unique
     _id: name,
   });
   return category.save();
@@ -114,6 +134,7 @@ exports.deleteCategory = async (name) => {
   return Category.deleteOne({_id:name}).exec();
 }
 
+// Get all the products from the database that has the specified category in its list of categories
 exports.getProductsFromCategory = async (category) => {
   let products = await Product.find({ categories: category}).exec();
   return products;
@@ -142,6 +163,8 @@ const nodemailer = require('nodemailer');
 
 // Sends an email from the mail account specified in config file.
 exports.sendEmail = (reqbody) => {
+  /* the transporter object is the service that is responsible for logging into a mail account
+      creating the email, and finally sending the email.  */
   const transporter = nodemailer.createTransport(config.transporter);
 
   const mailOptions = {
