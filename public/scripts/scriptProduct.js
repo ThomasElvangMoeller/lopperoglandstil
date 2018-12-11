7328df28c2a0030f5299690c685ece6403d0c07a
planktoon1 <fkoe90@gmail.com>
2018-12-11 10:55:36 +0100

    Reservation inside product display

--- public/scripts/scriptProduct.js
+++ public/scripts/scriptProduct.js
@@ -13,9 +13,54 @@ onload = async () => {
     const productTemplate = await fetch('/templates/productDescription.hbs');
     const productTemplateText = await productTemplate.text();
     const compiledProductTemplate = Handlebars.compile(productTemplateText);
-    document.getElementById("product-container").innerHTML = compiledProductTemplate(productIdJSON);
+    document.getElementById("product-container").innerHTML = await compiledProductTemplate(productIdJSON);
 
     // Imports script for footer
     navigation();
     footer();
+
+    // -------------- Reservation popup ----------------
+    const reserverButton = document.getElementById("reservation");
+    const reservationWindow = document.getElementById("reservationWindow");
+    document.getElementById("closeBtn").onclick = () => {reservationWindow.style.display = "none";}
+
+    const btnProduct = reserverButton.dataset.product;
+    reserverButton.onclick = () => {
+        reservationWindow.style.display = "block";
+        document.getElementById("reserverProduct").innerHTML = `Produkt: ${btnProduct}`;
+    }
+
+    const submitReservation = document.getElementById("submitReservation");
+
+    submitReservation.onclick = sendReservationMail;
 };
+
+// Send an request to server asking to send a notification to shop owner about a reservation
+async function sendReservationMail() {
+    const antal = document.getElementById("reserverAmount").value;
+    const email = document.getElementById("reserverEmail").value;
+    const telefonnr = document.getElementById("reserverPhone").value;
+    const kommentare = document.getElementById("reserverComments").value;
+    const produkt = document.getElementById("reserverProduct").innerHTML;
+
+    const postBody = {
+        from: email,
+        html: ` ${produkt} <br> Antal : '${antal}' <br> Email : '${email}' <br> Telefonnr : '${telefonnr}' <br> Kommentare: '${kommentare}'`
+    };
+
+    let status = await fetch('/api/email/reservation', {method: 'POST',
+                headers:{'Content-Type':'application/json'},
+                body:JSON.stringify(postBody)
+    });
+
+    let statusJSON = await status.json();
+
+    if(statusJSON.success){
+        document.getElementById('error').innerText = 'Reservation forespørgsel sendt';
+        setTimeout(function () {
+            location.reload();
+        }, 2000);
+    }else{
+        document.getElementById('error').innerText = 'Fejl: Reservation forespørgsel ikke sendt. Prøv igen';
+    }
+}
